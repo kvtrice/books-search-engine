0fa7afc094b45a6a718dad8edc9c6fd6cb16ab73
kvtrice <katricemountford@gmail.com>
2024-07-11 15:58:10 +1000

    Adds search bar to search google books api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { useEffect } from "react";
 import Header from "./components/Header/Header";
 import { fetchBooks } from "./services/books-service";
+import BookSearch from "./components/BookSearch/BookSearch";
+import BooksContainer from "./containers/BooksContainer/BooksContainer";
+import BookSearchContextProvider from "./contexts/BookSearchContextProvider";
 
 function App() {
-	useEffect(() => {
-		fetchBooks("gone girl", 5).then(data => console.log(data));
-	}, []);
+	// useEffect(() => {
+	// 	fetchBooks("gone girl", 5).then(data => console.log(data));
+	// }, []);
 
 	return (
 		<>
 			<Header />
+			<BookSearchContextProvider>
+				<BookSearch />
+				<BooksContainer />
+			</BookSearchContextProvider>
 		</>
 	);
 }
--- src/components/BookSearch/BookSearch.jsx
+++ src/components/BookSearch/BookSearch.jsx
@@ -1,6 +1,32 @@
+import { useContext, useRef, useState } from "react";
 import styles from "./BookSearch.module.scss";
+import { BookSearchContext } from "../../contexts/BookSearchContextProvider";
+import { fetchBooks } from "../../services/books-service";
 
 const BookSearch = () => {
-	return <div>BookSearch</div>;
+	const searchInputRef = useRef(null);
+	const { books, setBooks } = useContext(BookSearchContext);
+
+	const handleSearch = async e => {
+		e.preventDefault();
+		const searchQuery = searchInputRef.current.value;
+		const retrievedBooks = await fetchBooks(searchQuery, 10);
+		setBooks(retrievedBooks);
+		searchInputRef.current.value = "";
+	};
+
+	console.log(books);
+
+	return (
+		<form>
+			<input
+				type="text"
+				name="search"
+				id="search"
+				ref={searchInputRef}
+			/>
+			<button onClick={handleSearch}>Search</button>
+		</form>
+	);
 };
 export default BookSearch;
