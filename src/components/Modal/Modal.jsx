689d68e8b1c884584ab78cb45d905efe5531c202
kvtrice <katricemountford@gmail.com>
2024-07-14 14:20:21 +1000

    Adds functionaing modal with correct book details

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,32 @@
 import styles from "./Modal.module.scss";
 
-const Modal = () => {
-	return <div>Modal</div>;
+const Modal = ({ children, setIsShowModal }) => {
+	const closeModal = () => {
+		setIsShowModal(false);
+	};
+
+	return (
+		<div className={styles.modal__container}>
+			<div className={styles.modal}>
+				<div className={styles.modal__header}>
+					<p
+						className={styles.modal__header__close}
+						onClick={closeModal}
+					>
+						&times;
+					</p>
+				</div>
+				<div className={styles.modal__content}>{children}</div>
+				<div className={styles.modal__footer}>
+					<button
+						className={styles.modal__footer__btn}
+						onClick={closeModal}
+					>
+						Back
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 export default Modal;
--- src/containers/BooksContainer/BooksContainer.jsx
+++ src/containers/BooksContainer/BooksContainer.jsx
@@ -1,25 +1,40 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import styles from "./BooksContainer.module.scss";
 import { BookSearchContext } from "../../contexts/BookSearchContextProvider";
 import BookCard from "../../components/BookCard/BookCard";
 import { FetchStatusContext } from "../../contexts/FetchStatusContextProvider";
+import Modal from "../../components/Modal/Modal";
+import BookDetails from "../../components/BookDetails/BookDetails";
 
 const BooksContainer = () => {
 	const { books } = useContext(BookSearchContext);
 	const { fetchStatus } = useContext(FetchStatusContext);
+	const [isShowModal, setIsShowModal] = useState(false);
+	const [currentBook, setCurrentBook] = useState(null);
 
 	return (
-		<div>
-			{fetchStatus === "ERROR" && <p>No books found, please try another search term</p>}
-			{fetchStatus === "LOADING" && <p>...loading</p>}
-			{fetchStatus === "SUCCESS" &&
-				books?.map(book => (
-					<BookCard
-						key={book.id}
-						book={book}
-					/>
-				))}
-		</div>
+		<>
+			<div className={styles.container}>
+				{fetchStatus === "ERROR" && (
+					<p>No books found, please try another search term</p>
+				)}
+				{fetchStatus === "LOADING" && <p>...loading</p>}
+				{fetchStatus === "SUCCESS" &&
+					books?.map(book => (
+						<BookCard
+							key={book.id}
+							book={book}
+							setCurrentBook={setCurrentBook}
+							setIsShowModal={setIsShowModal}
+						/>
+					))}
+				{isShowModal && (
+					<Modal setIsShowModal={setIsShowModal}>
+						<BookDetails book={currentBook} />
+					</Modal>
+				)}
+			</div>
+		</>
 	);
 };
 
